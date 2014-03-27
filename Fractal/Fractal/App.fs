ce30f0e832a03f8fe873b8edc2d527e231fe0ce7
romaricf <hydraxisblue+github@gmail.com>
2014-03-27 18:49:02 +0100

    First commit

---
+++ Fractal/Fractal/App.fs
@@ -0,0 +1,74 @@
+﻿module MainApp
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Shapes
+open System.Windows.Media
+open FSharpx
+
+type MainWindow = XAML<"MainWindow.xaml">
+
+
+
+let loadWindow() =
+   let uiContext = System.Threading.SynchronizationContext.Current
+
+   let window = MainWindow()
+   
+   let canvas = window.GetChild("Canvas") :?> Canvas
+
+   let drawSquare(rect:Tree.Rect, level) = 
+        let mutable polygon = new Polygon()
+        let points = new PointCollection()
+
+        polygon.Stroke <- System.Windows.Media.Brushes.Black
+        polygon.StrokeThickness <- 0.0
+
+        let mp3_5 = Tree.squareLeftPoint(rect.p3,rect.p4)
+
+        let p1 = new Point(rect.p1.x, rect.p1.y)
+        let p2 = new Point(rect.p2.x, rect.p2.y)
+        let p3 = new Point(rect.p3.x, rect.p3.y)
+        let p4 = new Point(rect.p4.x, rect.p4.y)
+        let p3_5 = new Point(mp3_5.x, mp3_5.y)
+
+        points.Add(p1)
+        points.Add(p2)
+        points.Add(p3)
+        points.Add(p3_5)
+        points.Add(p4)
+
+        polygon.Points <- points
+        
+        match level with
+            | x when x<7 -> polygon.Fill <- System.Windows.Media.Brushes.Chocolate
+            | _ -> polygon.Fill <- System.Windows.Media.Brushes.DarkGreen
+
+        canvas.Children.Add(polygon) |> ignore
+        
+   
+   let rec drawPythagorasTree(rects, level) =
+        match level with
+        | 13 -> 0
+        | _ ->
+            List.iter (fun x -> drawSquare(x,level)) rects
+            let newRects = List.fold (fun l rect -> l @ (Tree.generateChildren rect)) [] rects
+            drawPythagorasTree(newRects,level+1)
+
+    
+    // drawing of the main tree
+   let drawTree() =
+        let size = 70.0
+        let oX = 270.0
+        let oY = 450.0
+        let rect1:Tree.Rect = { p1={x=oX;y=oY}; p3={x=oX-size;y=oY-size} }
+        let rects = [rect1]
+        drawPythagorasTree(rects,0)
+
+   drawTree() |> ignore
+
+   window.Root
+
+[<STAThread>]
+(new Application()).Run(loadWindow()) |> ignore
---
+++ Fractal/Fractal/PythagorasTree.fs
@@ -0,0 +1,47 @@
+﻿module Tree
+
+open System
+
+let sqrt2 = Math.Sqrt(2.0)
+
+type MyPoint = { x: float; y: float }
+
+let squareLeftPoint(p1,p3) =
+    let xc = (p1.x + p3.x)/2.0
+    let yc = (p1.y + p3.y)/2.0    // Center point
+    let xd = (p1.x - p3.x)/2.0
+    let yd = (p1.y - p3.y)/2.0    // Half-diagonal
+
+    let x2 = xc - yd
+    let y2 = yc + xd  
+    { x=x2;y=y2 }
+
+let squareRightPoint(p1,p3) =
+    let xc = (p1.x + p3.x)/2.0
+    let yc = (p1.y + p3.y)/2.0    // Center point
+    let xd = (p1.x - p3.x)/2.0
+    let yd = (p1.y - p3.y)/2.0    // Half-diagonal
+
+    let x4 = xc + yd
+    let y4 = yc - xd  
+    { x=x4;y=y4 }
+
+
+type Rect = {
+    p1: MyPoint; p3: MyPoint
+} with
+    member x.p2 = squareLeftPoint(x.p1,x.p3)
+    member x.p4 = squareRightPoint(x.p1,x.p3)
+
+let generateLeftRec(rect:Rect) =
+    {p1 = rect.p4; p3 = {x=rect.p4.x+(rect.p4.x - rect.p1.x);y=rect.p4.y+(rect.p4.y - rect.p1.y)}}
+
+let generateRightRec(rect:Rect) =
+    let p1 = rect.p3
+    let p3 = {x=rect.p3.x+(rect.p4.x - rect.p1.x);y=rect.p3.y+(rect.p4.y - rect.p1.y)}
+    {p1 = squareRightPoint(p1,p3); p3 = squareLeftPoint(p1,p3) }
+
+let generateChildren(rect) =
+        let leftRect = generateLeftRec(rect)
+        let rightRect = generateRightRec(rect)
+        [leftRect;rightRect]
